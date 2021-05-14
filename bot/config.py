1c3909ea85ad9c262da45b2f6b0cc42c4b23dc8c
krishnapatel445 <84168972+krishnapatel445@users.noreply.github.com>
2021-05-14 11:02:20 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,9 +1,11 @@
 class config:
-    BOT_TOKEN = ""
-    APP_ID = ""
-    API_HASH = ""
-    DATABASE_URL = ""
-    SUDO_USERS = "" # Sepearted by space.
+    BOT_TOKEN = "Kenil Patel, [14.05.21 10:52]
+1814619402:AAF0gwuqQTmSR-W0HZeLhnUqGZs3ewCprLo"
+    APP_ID = " 939847"
+    API_HASH = "8239960da0674fef61884844c0ba79b6
+ "
+    DATABASE_URL = "postgres://pbuvbcheekbbem:4728898858cf869ca9108488338f15e34e60e205d1d6d8835cd489589a53d1f3@ec2-18-215-111-67.compute-1.amazonaws.com:5432/decjq27ua7mf0h"
+    SUDO_USERS = " 664418878" # Sepearted by space.
     SUPPORT_CHAT_LINK = ""
     DOWNLOAD_DIRECTORY = "./downloads/"
     G_DRIVE_CLIENT_ID = ""
